9c2195bc15036f8f343050f1522eaff68fbc3991
Mark Sutton <msutto11@jhu.edu>
2017-11-21 17:58:16 -0500

    more database comment test cases

--- CADET/DataLayer/tests/dbtest/post_comment.sh
+++ CADET/DataLayer/tests/dbtest/post_comment.sh
@@ -1,1 +1,3 @@
 curl --request POST --data @comment1.json http://127.0.0.1:5000/api/Comment -H "Content-Type: application/json"
+curl --request POST --data @comment2.json http://127.0.0.1:5000/api/Comment -H "Content-Type: application/json"
+curl --request POST --data @comment3.json http://127.0.0.1:5000/api/Comment -H "Content-Type: application/json"
