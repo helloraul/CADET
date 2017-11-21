5095ac7ba194083228769f0fc86eb3bd7d6ed2eb
Jason Porter <jporte42@jhu.edu>
2017-11-21 17:46:14 -0500

    Removed old database folder from project
    Removed the old database folder from the project. This folder's contents
    mainly exists in the DataLayer/cadetapi/controller/database/ directory.

--- CADET/database/test_course.py
+++
@@ -1,9 +0,0 @@
-from cadet_insert import *
-
-# Test Case for inserting and/or fetching a Course
-incrs = DbCourse()
-incrsId = incrs.GetId('Computer Science', 3, '605.411')
-print('Course ID: ', incrsId)
-print('Now Check again')
-incrsId = incrs.GetId('Computer Science', 3, '605.411')
-print('Course ID: ', incrsId)
