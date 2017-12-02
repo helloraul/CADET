a2340cfc0fb1ab84330d890c60cd9ce052c6cae3
ccrogers <chase.c.rogers@gmail.com>
2017-12-02 16:54:35 -0500

    Somewhat improved test file for AnalysisModule
    The test is probably buggier than the AnalysisModule.
    Need to flesh out its test cases.
    Need to decouple the function tests.

--- CADET/DataLayer/tests/analysis_tests/test_AnalysisModule.py
+++ CADET/DataLayer/tests/analysis_tests/test_AnalysisModule.py
@@ -12,8 +12,10 @@ from cadetapi.controllers.analysis.Comment import Comment as CommentObject
 
 sentiments  = ['positive', 'negative', 'neutral']
 instructor_comment_objects = [
-        CommentObject(comment_dict = {'comment':'hello', 'comment_type':'instructor'}),
-        CommentObject(comment_dict = {'comment':'goodbye', 'comment_type':'instructor'}) ]
+        CommentObject(comment_dict = {'comment':'hello', 'comment_type':'instructor',
+            'instructor_first_name':'Who', 'instructor_last_name':'Cares'}),
+        CommentObject(comment_dict = {'comment':'goodbye', 'comment_type':'instructor',
+            'instructor_first_name':'Who', 'instructor_last_name':'Cares'}) ]
 
 course_comment_objects = [
         CommentObject(comment_dict = {'comment':'hello', 'comment_type':'course'}),
@@ -35,9 +37,8 @@ def test_separate_comment_types():
 # invalid results would occur if multiple different comments share a comment_id
 def test_sentiment_processing():
     analyzer = Analyzer()
-    analyzer.comment_objects = comment_objects
-    analyzer.separateCommentTypes()
-
+    analyzer.courseCommentList = course_comment_objects
+    analyzer.instructorCommentList = course_comment_objects
 
     # create separate thread for analyzing course comments
     if analyzer.courseCommentList is not None:
@@ -54,30 +55,29 @@ def test_sentiment_processing():
         analyzer.instructor_process = ctx2.Process(target=analyzer.processInstructorComments, args=(commentPipe, analyzer.instructorCommentList))
         analyzer.instructor_process.start()
         
-    course_sentiments = analyzer.qList.recv()
-    for comment_object in analyzer.courseCommentList:
-        comment_object.setSentiment(course_sentiments[comment_object.getCommentId()])
-    analyzer.sentiment_process.join()
-    analyzer.sentiment_process.terminate()
+    analyzer.updateAttributes()
 
-    data = analyzer.instructorListPipe.recv()
-    instructor_sentiments = data[0]
-    for comment_object in analyzer.instructorCommentList:
-        comment_object.setSentiment(instructor_sentiments[comment_object.getCommentId()])
-    analyzer.instructor_process.join()
-    analyzer.instructor_process.terminate()
-    
     # check if a sentiment is now attached
     for comment_object in analyzer.getCourseComments():
         assert comment_object.getSentiment() in sentiments
     for comment_object in analyzer.getInstructorComments():
         assert comment_object.getSentiment() in sentiments
 
+    # check that number of comments in histogram = number of instructor comments
+    instructor_sentiment_histogram = analyzer.getInstructorSentimentHistogram()
+    count = 0
+    for name in instructor_sentiment_histogram.keys():
+        count += instructor_sentiment_histogram.get(name)[0]
+        count += instructor_sentiment_histogram.get(name)[1]
+        count += instructor_sentiment_histogram.get(name)[2]
+    assert count == len(instructor_comment_objects)
+
 def test_topic_model():
     num_topics = 5
     num_words_per_topic = 5
     num_iterations = 5
     analyzer = Analyzer(num_topics = num_topics, words_per_topic=num_words_per_topic, iterations=num_iterations)
+    analyzer.courseCommentList = course_comment_objects
     analyzer.text = course_comment_text
     analyzer.processLDAModel()
     assert analyzer.ldamodel.num_topics == num_topics
@@ -87,27 +87,18 @@ def test_topic_model():
 
     analyzer.processTopicSentimentHistogram()
     topic_model = analyzer.getTopicModel()
+    topic_sentiment_histogram = analyzer.getTopicHistogram() 
     assert len(topic_model) == analyzer.ldamodel.num_topics
+
+    # check that number of comments in histogram = number of course comments
+    count = 0
     for i in range(0, len(topic_model)):
         assert len(topic_model[i]) == analyzer.ldamodel.num_terms
-    topic_sentiment_histogram = analyzer.getTopicHistogram()
-    
-    
+        count += topic_sentiment_histogram.get(i)[0]
+        count += topic_sentiment_histogram.get(i)[1]
+        count += topic_sentiment_histogram.get(i)[2]
+    count += topic_sentiment_histogram.get(-1)[0]
+    count += topic_sentiment_histogram.get(-1)[1]
+    count += topic_sentiment_histogram.get(-1)[2]
+    assert count == len(course_comment_objects)
 
-
-if __name__ == '__main__':
-
-    test_lda_model()
-    # test data
-
-    # test processSentiment
-
-    # test processInstructorComments
-
-    # test processLDAModel
-
-    # test updateAttributes
-
-    # test procesTopicSentimentHistgram
-
-    # test separateCommentTypes
