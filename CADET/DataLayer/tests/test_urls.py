51ec628ebd9b7fc2f42774228e2605b61541442e
Jason Porter <jporte42@jhu.edu>
2017-12-03 15:40:03 -0500

    Finally figured out the post method for testing!

--- CADET/DataLayer/tests/test_urls.py
+++ CADET/DataLayer/tests/test_urls.py
@@ -1,3 +1,5 @@
+"""Make sure the TestConfig is being used in __init__.py"""
+
 import os
 import cadetapi
 import unittest
@@ -9,7 +11,7 @@ class TestURLs(unittest.TestCase):
         self.client = cadetapi.app.test_client()
 
     def post_comment(self):
-        postdata = dict(anon_id = "42", \
+        postdata = json.dumps(dict(anon_id = "42", \
             instructor_first_name = "Joe",      \
             instructor_last_name = "Bennett",   \
             course_program = "Engineering",     \
@@ -17,7 +19,7 @@ class TestURLs(unittest.TestCase):
             course_num_sect_id = "605.423",     \
             course_comments = "I like this class overall", \
             instructor_comments = "The instructor did a good job of challenging the students", \
-            additional_comments = "The campus is in a really convenient location")
+            additional_comments = "The campus is in a really convenient location"))
 
         return self.client.post('/api/Comment', data=postdata, content_type='application/json')
 
@@ -41,11 +43,31 @@ class TestURLs(unittest.TestCase):
         result = self.client.get('/api/Comment/1')
         assert result.status_code == 204 # expecting the element to not exist
         assert result.headers['Content-Type'] == "application/json"
-
+        # post data to comment table
         result = self.post_comment()
         assert result.status_code == 200
         assert result.headers['Content-Type'] == "application/json"
         assert 'comment_id' in result.data
+        result = self.client.get('/api/Comment')
+        assert result.status_code == 200
+        assert result.headers['Content-Type'] == "application/json"
+        assert ""
+
+
+#    def test_result_return(self):
+#        """Tests the Result URL"""
+#        # check root result url
+#        result = self.client.get('/api/Result')
+#        assert result.status_code == 204 # expecting the table to be empty
+#        assert result.headers['Content-Type'] == "application/json"
+#        # check again with trailing slash
+#        result = self.client.get('/api/Result/')
+#        assert result.status_code == 204 # expecting the table to be empty
+#        assert result.headers['Content-Type'] == "application/json"
+#        # check element from Comment
+#        result = self.client.get('/api/Result/1')
+#        assert result.status_code == 204 # expecting the element to not exist
+#        assert result.headers['Content-Type'] == "application/json"
 
 if __name__ == '__main__':
     unittest.main()
